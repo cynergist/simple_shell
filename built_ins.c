fb8518e2b5f9e16b8255cae2d24b56b4ca0a9c04
Cyn <cmdominguez@gmail.com>
2019-04-18 06:47:33 +0000

    adding getenv

--- built_ins.c
+++ built_ins.c
@@ -17,3 +17,40 @@ int _printenv(void)
 	}
 	return (0);
 }
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string to compare to first string
+ *
+ * Return: <0 if s1 is less than s2, 0 for equal, >0 if s1 is greater than s2
+ */
+int _strncmp(char *s1, char *s2, int n)
+{
+	while ((*s1 == *s2) && (n--))
+	{
+	if (*s1 == '\0')
+	{
+	return (0);
+	}
+	s1++;
+	s2++;
+	}
+	return (*s1 != *s2);
+}
+/**                                                                                                
+ * _getenv - prints environment like getenv                                                        
+ *                                                                                                 
+ * Return: 0                                                                                       
+ */
+char * _getenv(char * var)
+{
+ 	int i = 0;
+
+	for (i = 0; environ[i]; i++)
+	{
+		if (strncmp(environ[i], var, _strlen(var)) == 0)
+			return (&environ[i][_strlen(var)]);
+	}
+	return (NULL);
+}
